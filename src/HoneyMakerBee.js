de183aaec4df3ac67148670c17fd24803faf1e0d
Max <maxmillianrwoods@gmail.com>
2019-06-03 12:07:25 -0700

    This commit allows all tests to pass

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,18 @@
 var HoneyMakerBee = function() {
+	this.age = 10;
+	this.job = 'make honey';
+	this.honeyPot = 0;
 };
+
+HoneyMakerBee.prototype = Object.create(new Bee());
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function () {
+	return this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function () {
+	return this.honeyPot--;
+};
+
 
